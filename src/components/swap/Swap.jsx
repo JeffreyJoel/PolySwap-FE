ac0a4001f2b899992e562bdefe7fb747f36bfcd1
JeffreyJoel <jowoloko@gmail.com>
2024-03-29 17:54:53 +0100

    update ui

--- src/components/swap/Swap.jsx
+++ src/components/swap/Swap.jsx
@@ -1,43 +1,8 @@
 "use client";
 
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import { useForm } from "react-hook-form";
-// import { IoWalletOutline } from "react-icons/io5";
-// import * as z from "zod";
-
-// import { useAccount, useContractRead } from "wagmi";
-// import WCX_Token_ABI from "@/abi/wcx-token-abi.json";
-
 import { Button } from "@/components/ui/button";
-import { Input } from "../ui/input";
-// import {
-//   Form,
-//   FormControl,
-//   FormField,
-//   FormItem,
-//   FormMessage,
-// } from "@/components/ui/form";
-// import {
-//   Select,
-//   SelectContent,
-//   SelectItem,
-//   SelectTrigger,
-//   SelectValue,
-// } from "@/components/ui/select";
-
-// import { Input } from "@/components/ui/input";
-// import { stakeSchema } from "@/lib/validators";
-// import { def } from "@/lib/utils";
 
 export default function Swap() {
-  //   const account = useAccount();
-  //   const result = useContractRead({
-  //     abi: WCX_Token_ABI,
-  //     address: "0xbFf157FE56a966208b00f1cb3699dF7601a13a08",
-  //     functionName: "balanceOf",
-  //     args: [account?.address],
-  //   });
-
   return (
     <div className="w-full flex items-center justify-center">
       <div className="p-0 md:p-6 pb-4 bg-background md:border md:shadow-md rounded-2xl w-full max-w-2xl">
@@ -45,31 +10,11 @@ export default function Swap() {
           <h2 className="text-xl md:text-2xl font-semibold">Transfer</h2>
 
           <div className="flex items-center gap-x-1">
-            <p className="flex items-center mr-2">
-              {/* <IoWalletOutline className="w-5 h-5 text-primary mr-1" /> */}
-              {/* <span className="font-medium text-sm">WCX</span> */}
-            </p>
-{/* 
-            <Button
-              size="sm"
-              variant="secondary"
-              className="py-2 h-7 font-bold rounded-full shadow"
-            >
-              Max
-            </Button>
-            <Button
-              size="sm"
-              variant="secondary"
-              className="py-2 h-7 font-bold rounded-full shadow"
-            >
-              Half
-            </Button> */}
+            <p className="flex items-center mr-2"></p>
+      
           </div>
         </div>
-
-        {/* <Form {...form}> */}
         <form
-          // onSubmit={form.handleSubmit(onSubmit)}
           className="my-5 flex flex-col gap-4"
         >
           <div className="rounded-full p-3 pl-5 flex items-center border h-16">
